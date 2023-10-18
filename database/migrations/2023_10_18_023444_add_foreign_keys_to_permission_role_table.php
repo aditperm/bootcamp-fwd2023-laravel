7e6648b9cb62ec1f8bcc8ef1e1a464bcbfe34f41
aditperm <adityapermana3275@gmail.com>
2023-10-18 10:52:00 +0700

    update migration foreign keys final

--- database/migrations/2023_10_18_023444_add_foreign_keys_to_permission_role_table.php
+++ database/migrations/2023_10_18_023444_add_foreign_keys_to_permission_role_table.php
@@ -14,7 +14,14 @@ class AddForeignKeysToPermissionRoleTable extends Migration
     public function up()
     {
         Schema::table('permission_role', function (Blueprint $table) {
-            //
+            $table->foreign('permission_id', 'fk_permission_role_to_permission')
+            ->references('id')->on('permission')->onDelete('CASCADE')
+            ->onUpdate('CASCADE'); //dari table user id ke table users
+            //cascade = diperbolehkan akses nya
+            //strict = tidak boleh dihapus
+            $table->foreign('role_id', 'fk_permission_role_to_role')
+            ->references('id')->on('role')->onDelete('CASCADE')
+            ->onUpdate('CASCADE');
         });
     }
 
@@ -26,7 +33,8 @@ class AddForeignKeysToPermissionRoleTable extends Migration
     public function down()
     {
         Schema::table('permission_role', function (Blueprint $table) {
-            //
+            $table->dropForeign('fk_permission_role_to_permission');
+            $table->dropForeign('fk_permission_role_to_role');
         });
     }
 }
